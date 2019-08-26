bdd783b847a1c5a2cb1efca4eb619fddd30dbe32
Walter Melo <walmello24@gmail.com>
2019-08-26 15:17:45 -0300

    rewritten almost everything to fix some nasty bugs, and give a better GUI to the calculator

--- script.js
+++ script.js
@@ -1,68 +1,121 @@
-// Arithmetic functions
-
-function add(n1, n2) {
+function add(n1, n2){
     return n1 + n2
 }
 
-function sub(n1, n2) {
+function sub(n1, n2){
     return n1 - n2
 }
 
-function mult(n1, n2) {
+function mult(n1, n2){
     return n1 * n2
 }
 
-function div(n1, n2) {
-    return n1 / n2 
+function div(n1, n2){
+    return n1 / n2
 }
 
-function operate(n1,op,n2) {
-    n1 = parseInt(n1)
-    n2 = parseInt(n2)
+function operate(n1, op, n2){
+    n1 = parseFloat(n1)
+    n2 = parseFloat(n2)
+
+    result = 0
+
     switch(op){
         case '+':
-            return add(n1,n2)
+            result = add(n1,n2)
+            break
         case '-':
-            return sub(n1,n2)
+            result = sub(n1,n2)
+            break
         case '*':
-            return mult(n1,n2)
+            result = mult(n1, n2)
+            break
         case '/':
-            return div(n1,n2)
+            result = div(n1, n2)
+            break
     }
+    return result
 }
 
-// caluculator GUI
+let buttons = {
+    numbers: document.querySelectorAll('.number'),
+    operators: document.querySelectorAll('.operator'),
+    equal: document.querySelector('#equal')
+}
 
-let input =
-    document.querySelector('input[type="text"]')
-let buttons = 
-    document.querySelectorAll('button')
-let dotButton = 
-    document.querySelector('#dot') 
-let addButton =
-    document.querySelector('#add')
-let eqlButton =
-    document.querySelector('#eql')
+let display = {
+    GUI: document.querySelector("#display"),
+    add(n){
+        this.GUI.innerHTML += n
+    },
+    set(n){
+       this.GUI.innerHTML = n 
+    },
+    get(){
+        return this.GUI.innerHTML
+    },
+    clearMode: false
+}
 
-buttons.forEach(button => {
-    let id = button.id
-    if(id[0] == 'n')
-        button.onclick = function(){
-            if(input.value.includes('.'))
-                input.value += button.innerHTML
-            else if(input.value == '0')
-                input.value = button.innerHTML
-            else
-                input.value += button.innerHTML 
+buttons.numbers.forEach(n => {
+    let digit = n.innerHTML
+    n.onclick = function(){
+        if(display.clearMode){
+            display.set(0)
+            display.clearMode = false
         }
-    if(id == 'n0')
-        button.onclick = function(){
-            if(input.value != 0)
-                input.value += '0'
+
+        if(n.innerHTML == 0){
+            if(display.get() != 0){
+                display.add(digit)
+            }
         }
-    if(id == 'dot')
-        button.onclick = function(){
-            if(!input.value.includes('.'))
-                input.value += '.'
+        else{
+            if(display.get() == 0){
+                display.set(digit)
+            }
+            else{
+                display.add(digit)
+            }
         }
+    }
 })
+
+let store = {
+    value: display.get(),
+    operator: ''
+}
+
+buttons.operators.forEach(op => {
+    let signal = op.innerHTML
+    op.onclick = function(){
+        store = {
+            value: display.get(),
+            operator: signal
+        }
+        display.clearMode = true
+        console.log(store)
+    }
+})
+
+buttons.equal.onclick = function(){
+    if(store.operator == '') return
+    let before = display.get()
+    let value = operate(
+        store.value,
+        store.operator,
+        display.get()
+    )
+    if(!display.clearMode){
+        store.value = Math.abs(before)
+        display.clearMode = true
+        display.set(value)
+    }
+    else{
+        if(store.operator == '-'){
+            value *= -1
+        }
+    }
+
+    display.set(value)
+}
